070a9415b37ec233837ee2d34741f4cfe4a8c350
cafloivedu <cafloivedu@outlook.com>
2020-04-14 17:43:09 -0500

    halfway

---
+++ lib/model/todo2.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NewTodoDialog extends StatefulWidget {
+  @override
+  NewTodoDialogState createState() => NewTodoDialogState();
+}
+
+class NewTodoDialogState extends State<NewTodoDialog> {
+  final controllerTitle = new TextEditingController();
+  final controllerBody = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[200],
+      title: Text(
+        'New todo',
+        style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 20.0),
+      ),
+      content: Column(children: <Widget>[
+        FlatButton(
+            child: Text('Regret'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+      ])
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:f_202010_todo_class/model/todo.dart';
+import 'package:f_202010_todo_class/model/todo2.dart';
 import 'package:flutter/material.dart';
 
 class HomePageTodo extends StatefulWidget {
@@ -26,7 +27,47 @@ class _HomePageTodoState extends State<HomePageTodo> {
       itemCount: todos.length,
       itemBuilder: (context, posicion) {
         var element = todos[posicion];
-        return _item(element, posicion);
+        return Dismissible(
+            key: UniqueKey(),
+            onDismissed: (direction) {
+              //_item.removeAt(index);
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text("Item dismissed"),
+                )
+              );
+            },
+            
+            background: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Eliminar",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              color: Colors.redAccent, 
+            ),
+            
+            secondaryBackground: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text("Eliminar",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              color: Colors.redAccent, 
+            ),
+
+            
+            child: Card(
+              color: element.completed == 1 ? Colors.blue[200]:Colors.yellow[200],
+              child: ListTile(
+                title: _item(element, posicion),
+                subtitle: Text(""),
+              ),
+            ),
+          );
       },
     );
   }
@@ -40,4 +81,12 @@ class _HomePageTodoState extends State<HomePageTodo> {
       todos.add(new Todo(title:"itemT", body: "itemB", completed: 0 ));
     });
   }
+
+  // void _onTap(BuildContext context, Todo location, int posicion) {
+  //   setState(() {
+  //     if (this.widget.todos[posicion].completed == 0) {
+  //       this.widget.todos[posicion].completed = 1;
+  //     }
+  //   });
+  // }
 }
