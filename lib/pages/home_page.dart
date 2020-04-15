1a046d821d7aa5d4fab5522ef05d685e7e415672
cafloivedu <cafloivedu@outlook.com>
2020-04-14 20:18:05 -0500

    Ahora es tu turno

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePageTodo extends StatefulWidget {
 
 class _HomePageTodoState extends State<HomePageTodo> {
   List<Todo> todos = new List<Todo>();
-
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,68 +18,120 @@ class _HomePageTodoState extends State<HomePageTodo> {
       ),
       body: _list(),
       floatingActionButton: new FloatingActionButton(
-          onPressed: _addTodo, tooltip: 'Add task', child: new Icon(Icons.add)),
+          onPressed: _popUp, tooltip: 'Add task', child: new Icon(Icons.add)),
     );
   }
 
-  Widget _list(){
-      return ListView.builder(
+  Widget _list() {
+    return ListView.builder(
       itemCount: todos.length,
       itemBuilder: (context, posicion) {
         var element = todos[posicion];
         return Dismissible(
-            key: UniqueKey(),
-            onDismissed: (direction) {
-              //_item.removeAt(index);
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text("Item dismissed"),
-                )
-              );
-            },
-            
-            background: Container(
-              padding: EdgeInsets.all(20.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Eliminar",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              color: Colors.redAccent, 
-            ),
-            
-            secondaryBackground: Container(
-              padding: EdgeInsets.all(20.0),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Text("Eliminar",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              color: Colors.redAccent, 
-            ),
-
-            
-            child: Card(
-              color: element.completed == 1 ? Colors.blue[200]:Colors.yellow[200],
-              child: ListTile(
-                title: _item(element, posicion),
-                subtitle: Text(""),
+          key: UniqueKey(),
+          onDismissed: (direction) {
+            //_item.removeAt(index);
+            Scaffold.of(context).showSnackBar(SnackBar(
+              content: Text("Item dismissed"),
+            ));
+          },
+          background: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Eliminar",
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          );
+            color: Colors.redAccent,
+          ),
+          secondaryBackground: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                "Eliminar",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            color: Colors.redAccent,
+          ),
+          child: Card(
+            color:
+                element.completed == 1 ? Colors.blue[200] : Colors.yellow[200],
+            child: ListTile(
+              title: _item(element, posicion),
+              subtitle: Text(""),
+            ),
+          ),
+        );
       },
     );
   }
 
-  Widget _item(Todo element, int posicion){
+  Widget _item(Todo element, int posicion) {
     return Text('$posicion');
   }
 
-  void _addTodo(){
+  void _addTodo() {
     setState(() {
-      todos.add(new Todo(title:"itemT", body: "itemB", completed: 0 ));
+      todos.add(new Todo(title: "itemT", body: "itemB", completed: 0));
     });
+  }
+
+  void _popUp() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.yellow[200],
+            title: Text(
+              'New todo',
+              style: TextStyle(
+                  color: Theme.of(context).primaryColor, fontSize: 20.0),
+            ),
+            content: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                    ),
+                    //controller: controllerTitle,
+                    autofocus: true,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Body',
+                    ),
+                    //controller: controllerTitle,
+                    autofocus: true,
+                  ),
+                  // Expanded(
+                  //   child: TodoTypeDropdown,
+                  // )
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 
   // void _onTap(BuildContext context, Todo location, int posicion) {
